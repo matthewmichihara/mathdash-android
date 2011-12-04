f2ba821201794b48f552e943fe475b15d7700493
Matthew Michihara <matthew.michihara@gmail.com>
2011-12-04 01:46:38 -0800

    Added random equation, score, and timer to GameActivity

--- mathdash-android/src/com/fourpool/GameActivity.java
+++ mathdash-android/src/com/fourpool/GameActivity.java
@@ -2,12 +2,73 @@ package com.fourpool;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.fourpool.model.RandomEquation;
 
 public class GameActivity extends Activity {
+	
+	private int correct = 0;
+	private int total = 0;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game);
+        
+        // Set countdown
+        new CountDownTimer(30000, 1000) {
+        	public void onTick(long millisUntilFinished) {
+        		TextView timeRemainingTextView = (TextView)findViewById(R.id.gameTimeRemaining);
+        		timeRemainingTextView.setText(Long.toString(millisUntilFinished/1000));
+        	}
+        	
+        	public void onFinish() {
+        		TextView timeRemainingTextView = (TextView)findViewById(R.id.gameTimeRemaining);
+        		timeRemainingTextView.setText("Done");
+        	}
+        }.start();
+        
+        refreshEquation();
+    }
+    
+    private void refreshEquation() {
+    	final RandomEquation randomEquation = new RandomEquation();
+        
+        TextView answerTextView = (TextView)findViewById(R.id.gameEquation);
+        answerTextView.setText(randomEquation.equation);
+        
+        View.OnClickListener clickListener = new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				total += 1;
+				if (((Button)v).getText() == Integer.toString(randomEquation.answer))
+					correct += 1;
+				
+				TextView solvedTextView = (TextView)findViewById(R.id.gameSolved);
+				solvedTextView.setText(correct + "/" + total);
+				refreshEquation();
+			}
+		};
+        
+        Button gameButton1 = (Button)findViewById(R.id.gameButton1);
+        gameButton1.setText(Integer.toString(randomEquation.answer1));
+        gameButton1.setOnClickListener(clickListener);
+        
+        Button gameButton2 = (Button)findViewById(R.id.gameButton2);
+        gameButton2.setText(Integer.toString(randomEquation.answer2));
+        gameButton2.setOnClickListener(clickListener);
+        
+        Button gameButton3 = (Button)findViewById(R.id.gameButton3);
+        gameButton3.setText(Integer.toString(randomEquation.answer3));
+        gameButton3.setOnClickListener(clickListener);
+        
+        Button gameButton4 = (Button)findViewById(R.id.gameButton4);
+        gameButton4.setText(Integer.toString(randomEquation.answer4));
+        gameButton4.setOnClickListener(clickListener);
     }
 }
---
+++ mathdash-android/src/com/fourpool/model/RandomEquation.java
@@ -0,0 +1,75 @@
+package com.fourpool.model;
+
+import java.util.Random;
+
+public class RandomEquation {
+
+	public String equation;
+	public int answer;
+	public int answer1;
+	public int answer2;
+	public int answer3;
+	public int answer4;
+
+	private char[] operators = new char[] { '+', '-', '*', '/' };
+
+	public RandomEquation() {
+		Random random = new Random();
+		int r = random.nextInt(4);
+		char operator = operators[r];
+
+		int first = random.nextInt(15);
+		int second = random.nextInt(15);
+
+		equation = first + " " + operator + " " + second;
+
+		switch (operator) {
+		case '+':
+			answer = first + second;
+			break;
+		case '-':
+			answer = first - second;
+			break;
+		case '*':
+			answer = first * second;
+			break;
+		case '/':
+			while (first == 0 && second == 0) {
+				first = random.nextInt(15);
+				second = random.nextInt(15);
+			}
+
+			int dividend = first * second;
+
+			if (first == 0) {
+				equation = dividend + " / " + second;
+				answer = first;
+			} else {
+				equation = dividend + " / " + first;
+				answer = dividend / first;
+			}
+			break;
+		}
+
+		int[] answers = new int[4];
+		int randomIndex = random.nextInt(4);
+		answers[randomIndex] = answer;
+		int offset = 1;
+
+		while (randomIndex - offset >= 0) {
+			answers[randomIndex - offset] = answer - offset;
+			offset++;
+		}
+
+		offset = 1;
+		while (randomIndex + offset < 4) {
+			answers[randomIndex + offset] = answer + offset;
+			offset++;
+		}
+
+		answer1 = answers[0];
+		answer2 = answers[1];
+		answer3 = answers[2];
+		answer4 = answers[3];
+	}
+}
